74e16286493158eac365ffb31b5a6644c2dc6f2e
gisecalderon <gisecalderon08@gmail.com>
2023-05-28 17:29:49 -0300

    1

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import express from 'express';
+import handlebars from 'express-handlebars';
+import __dirname from './utils.js';
+
+const app = express();
+const PORT = process.env.PORT||8080;
+const server = app.listen(PORT,()=>console.log('Listening on ${PORT}'));
+
+app.engine('handlebars', handlebars.engine());
+app.set ('views', `${__dirname}/views)` ;
+app.set ('view engine','handlebars');
