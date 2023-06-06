202d6b7f4b77cf6e245b899beb1aaf66f24ce035
gisecalderon <gisecalderon08@gmail.com>
2023-06-05 22:20:12 -0300

    1paso

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import handlebars from 'express-handlebars';
+
+import viewsRouter from './routes/views.router.js';
 import __dirname from './utils.js';
 
 const app = express();
@@ -9,3 +11,9 @@ const server = app.listen(PORT,()=>console.log('Listening on ${PORT}'));
 app.engine('handlebars', handlebars.engine());
 app.set ('views', `${__dirname}/views`);
 app.set ('view engine','handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(express.static(`${__dirname}/public`));
+
+app.use('/', viewsRouter);
